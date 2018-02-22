5518445006657ca98561b9687b4f93b2222ac528
skif-cezar <33913952+skif-cezar@users.noreply.github.com>
2018-02-22 21:34:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var s = equation.split('*').join().split('x').join().split('^2').join().split(',');
+var res = s.filter(function(n){
+  if(n != '' && n != ' ') return n;
+});
+var a = parseInt(res[0].replace(/\s+/g,''));
+var b = parseInt(res[1].replace(/\s+/g,''));
+var c = parseInt(res[2].replace(/\s+/g,''));
+var D = Math.pow(b,2) - 4*a*c;
+var znam = 2*a;
+var x1 = Math.round((-b - Math.sqrt(D))/znam);
+var x2 = Math.round((-b + Math.sqrt(D))/znam);
+var arr = [x1, x2];
+function Num(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+arr.sort(Num);
+return arr;
 }
